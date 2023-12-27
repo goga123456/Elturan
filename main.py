ecfed59378fcb94a4d85cb164f50e3b1b28341b4
goga123456 <110159417+goga123456@users.noreply.github.com>
2023-12-27 13:30:49 +0500

    Update main.py

--- main.py
+++ main.py
@@ -12,14 +12,31 @@ from markups.reply_markups_start_and_back import *
 from states import ProfileStatesGroup
 import os
 import asyncpg
+from aiogram.utils.executor import start_webhook
+import logging
+
 
 from db import Database
+CHANNEL_ID = -1002018175768
+
 
 storage = MemoryStorage()
-bot = Bot(TOKEN_API)
+TOKEN = os.getenv('BOT_TOKEN')
+bot = Bot(token=TOKEN)
 dp = Dispatcher(bot,
                 storage=storage)
-CHANNEL_ID = -1001809493528
+
+HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME')
+
+# webhook settings
+WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
+WEBHOOK_PATH = f'/webhook/{TOKEN}'
+WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
+
+# webserver settings
+WEBAPP_HOST = '0.0.0.0'
+WEBAPP_PORT = os.getenv('PORT', default=8000)
+
 baza = Database()
 scheduler = AsyncIOScheduler()
 
@@ -261,7 +278,20 @@ async def edu_keyboard(callback_query: types.CallbackQuery, state: FSMContext):
             await ProfileStatesGroup.number_of_incident.set()
 
 
+async def on_startup(dispatcher):
+    await bot.set_webhook(WEBHOOK_URL, drop_pending_updates=True, max_connections=100)
+
+async def on_shutdown(dispatcher):
+    await bot.delete_webhook()
+
 if __name__ == '__main__':
-    scheduler.start()
-    executor.start_polling(dp,
-                           skip_updates=True)
+    logging.basicConfig(level=logging.INFO)
+    start_webhook(
+        dispatcher=dp,
+        webhook_path=WEBHOOK_PATH,
+        skip_updates=True,
+        on_startup=on_startup,
+        on_shutdown=on_shutdown,
+        host=WEBAPP_HOST,
+        port=WEBAPP_PORT,
+    )
