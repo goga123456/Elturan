9b5006869b634e26f56a4e4a35b43fb55e073838
goga123456 <110159417+goga123456@users.noreply.github.com>
2023-12-27 13:38:54 +0500

    Update db.py

--- db.py
+++ db.py
@@ -1,58 +1,65 @@
-import aiosqlite3
 import asyncpg
 import os
 
-
 class Database:
-    #def __init__(self, db_file):
-    #    self.db_file = db_file
-
     def __init__(self):
         self.db_url = os.environ['DATABASE_URL']
 
+    async def connect(self):
+        return await asyncpg.connect(dsn=self.db_url)
+
     async def insert(self, inc_number, inc_category, desc, priority, status):
-        async with aiosqlite3.connect(self.db_url) as conn:
-            cursor = await conn.cursor()
-            await cursor.execute(
-                'INSERT INTO incidents (inc_number, inc_category, desc, priority, status) VALUES (?, ?, ?, ?, ?);',
-                (inc_number, inc_category, desc, priority, status))
-            await conn.commit()
+        conn = await self.connect()
+        try:
+            await conn.execute(
+                'INSERT INTO incidents (inc_number, inc_category, desc, priority, status) VALUES ($1, $2, $3, $4, $5);',
+                inc_number, inc_category, desc, priority, status)
+        finally:
+            await conn.close()
 
     async def incidents(self):
-        async with aiosqlite3.connect(self.db_url) as conn:
-            cursor = await conn.cursor()
-            result = await cursor.execute('SELECT inc_number FROM incidents')
-            return result.fetchall()
+        conn = await self.connect()
+        try:
+            result = await conn.fetch('SELECT inc_number FROM incidents')
+            return result
+        finally:
+            await conn.close()
 
     async def select_incident(self, inc_number):
-        async with aiosqlite3.connect(self.db_url) as conn:
-            cursor = await conn.cursor()
-            result = await cursor.execute('SELECT * FROM incidents WHERE inc_number = ?', (inc_number,))
-            return result.fetchone()
+        conn = await self.connect()
+        try:
+            result = await conn.fetchrow('SELECT * FROM incidents WHERE inc_number = $1', inc_number)
+            return result
+        finally:
+            await conn.close()
 
     async def select_priority(self, inc_number):
-        async with aiosqlite3.connect(self.db_url) as conn:
-            cursor = await conn.cursor()
-            result = await cursor.execute('SELECT priority FROM incidents WHERE inc_number = ?', (inc_number,))
-            return result.fetchone()
+        conn = await self.connect()
+        try:
+            result = await conn.fetchrow('SELECT priority FROM incidents WHERE inc_number = $1', inc_number)
+            return result
+        finally:
+            await conn.close()
 
     async def delete_incident(self, inc_number):
-        async with aiosqlite3.connect(self.db_url) as conn:
-            cursor = await conn.cursor()
-            await cursor.execute('DELETE FROM incidents WHERE inc_number = ?', (inc_number,))
-            await conn.commit()
+        conn = await self.connect()
+        try:
+            await conn.execute('DELETE FROM incidents WHERE inc_number = $1', inc_number)
+        finally:
+            await conn.close()
 
     async def update_status(self, status, inc_number):
-        async with aiosqlite3.connect(self.db_url) as conn:
-            cursor = await conn.cursor()
-            await cursor.execute("UPDATE incidents SET status = ? WHERE inc_number = ?", (status, inc_number))
-            await conn.commit()
+        conn = await self.connect()
+        try:
+            await conn.execute("UPDATE incidents SET status = $1 WHERE inc_number = $2", status, inc_number)
+        finally:
+            await conn.close()
 
     async def update_priority(self, priority, inc_number):
-        async with aiosqlite3.connect(self.db_url) as conn:
-            cursor = await conn.cursor()
-            await cursor.execute("UPDATE incidents SET priority = ? WHERE inc_number = ?", (priority, inc_number))
-            await conn.commit()
-
+        conn = await self.connect()
+        try:
+            await conn.execute("UPDATE incidents SET priority = $1 WHERE inc_number = $2", priority, inc_number)
+        finally:
+            await conn.close()
 
 
