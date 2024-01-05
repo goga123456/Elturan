8c2fe2a9c22eb1eaafc528589bf98b09c628e300
goga123456 <110159417+goga123456@users.noreply.github.com>
2024-01-05 13:55:58 +0500

    Update main.py

--- main.py
+++ main.py
@@ -159,17 +159,17 @@ async def load_it_info(message: types.Message, state: FSMContext) -> None:
 async def edu_keyboard(callback_query: types.CallbackQuery, state: FSMContext):
     async with state.proxy() as data:
         data['choose'] = callback_query.data
-        date = await baza.select_closed_incident(data['choose'])
+        dates = await baza.select_closed_incident(data['choose'])
         await bot.send_message(CHANNEL_ID, f"Инцидент открыт заново\n"
-                                           f"Номер инцидента: {date[1]}\n"
-                                           f"Приоритет: {date[4]}\n"
-                                           f"Категория: {date[2]}\n"
-                                           f"Описание: {date[3]}\n")
-        await baza.insert(date[1], date[2], date[3], date[4], 'Открыт')
+                                           f"Номер инцидента: {dates[1]}\n"
+                                           f"Приоритет: {dates[4]}\n"
+                                           f"Категория: {dates[2]}\n"
+                                           f"Описание: {dates[3]}\n")
+        await baza.insert(dates[1], dates[2], dates[3], dates[4], 'Открыт')
         await baza.delete_incident_from_deleted(data['choose'])
         await callback_query.message.delete()
         await bot.send_message(chat_id=callback_query.message.chat.id,
-                               text=f"Инцидент с номером {data['choose']} открыт повторно",
+                               text=f"Инцидент с номером {data['choose']} открыт заново",
                                reply_markup=create_incident_kb())
 
         run_time = datetime.now() + timedelta(seconds=5)
@@ -178,44 +178,44 @@ async def edu_keyboard(callback_query: types.CallbackQuery, state: FSMContext):
         run_time3 = datetime.now() + timedelta(seconds=30)
         run_time4 = datetime.now() + timedelta(seconds=40)
         run_time5 = datetime.now() + timedelta(seconds=50)
-        if date[4] == 1:
+        if dates[4] == 1:
             msg = await bot.send_message(CHANNEL_ID, "@IsmoilovOybek")
             message_id = msg.message_id
             scheduler.add_job(delete_msg, "date", run_date=run_time,
                               args=[message_id],
                               max_instances=1)
             job=scheduler.add_job(prosrochen, "date", run_date=run_time1,
-                              args=[date[1], date[4], date[2], date[3]],
+                              args=[dates[1], dates[4], dates[2], dates[3]],
                               max_instances=1)
             scheduled_tasks[data['number']] = job
-        if date[4] == 2:
+        if dates[4] == 2:
             msg = await bot.send_message(CHANNEL_ID, "@Elturan")
             message_id = msg.message_id
             scheduler.add_job(delete_msg, "date", run_date=run_time,
                               args=[message_id],
                               max_instances=1)
             job=scheduler.add_job(prosrochen, "date", run_date=run_time2,
-                              args=[date[1], date[4], date[2], date[3]],
+                              args=[dates[1], dates[4], dates[2], dates[3]],
                               max_instances=1)
             scheduled_tasks[data['number']] = job
-        if date[4] == 3:
+        if dates[4] == 3:
             msg = await bot.send_message(CHANNEL_ID, "@Elturan")
             message_id = msg.message_id
             scheduler.add_job(delete_msg, "date", run_date=run_time,
                               args=[message_id],
                               max_instances=1)
             job=scheduler.add_job(prosrochen, "date", run_date=run_time3,
-                              args=[date[1], date[4], date[2], date[3]],
+                              args=[dates[1], dates[4], dates[2], dates[3]],
                               max_instances=1)
             scheduled_tasks[data['number']] = job
-        if date[4] == 4:
+        if dates[4] == 4:
             job=scheduler.add_job(prosrochen, "date", run_date=run_time4,
-                              args=[date[1], date[4], date[2], date[3]],
+                              args=[dates[1], dates[4], dates[2], dates[3]],
                               max_instances=1)
             scheduled_tasks[data['number']] = job
-        if date[4] == 5:
+        if dates[4] == 5:
             job=scheduler.add_job(prosrochen, "date", run_date=run_time5,
-                              args=[date[1], date[4], date[2], date[3]],
+                              args=[dates[1], dates[4], dates[2], dates[3]],
                               max_instances=1)
             scheduled_tasks[data['number']] = job
 
@@ -225,23 +225,21 @@ async def edu_keyboard(callback_query: types.CallbackQuery, state: FSMContext):
 async def edu_keyboard(callback_query: types.CallbackQuery, state: FSMContext):
     async with state.proxy() as data:
         data['choose'] = callback_query.data
-        dates = await baza.select_incident(data['choose'])
-
+        date = await baza.select_incident(data['choose'])
         await bot.send_message(CHANNEL_ID, f"Инцидент закрыт\n"
-                                           f"Номер инцидента: {dates[1]}\n"
-                                           f"Приоритет: {dates[4]}\n"
-                                           f"Категория: {dates[2]}\n"
-                                           f"Описание: {dates[3]}\n")
-        await baza.insert_deleted(dates[1], dates[2], dates[3], dates[4], 'Закрыт')
+                                           f"Номер инцидента: {date[1]}\n"
+                                           f"Приоритет: {date[4]}\n"
+                                           f"Категория: {date[2]}\n"
+                                           f"Описание: {date[3]}\n")
+        await baza.insert_deleted(date[1], date[2], date[3], date[4], 'Закрыт')
         await baza.delete_incident(data['choose'])
         await callback_query.message.delete()
         await bot.send_message(chat_id=callback_query.message.chat.id,
                                text=f"Инцидент с номером {data['choose']} закрыт",
                                reply_markup=create_incident_kb())
-        #if dates[1] in scheduled_tasks:
-        #    scheduled_tasks[dates[1]].remove()
-        #    del scheduled_tasks[dates[1]]
-          
+        if date[1] in scheduled_tasks:
+            scheduled_tasks[date[1]].remove()
+            del scheduled_tasks[date[1]]
         await ProfileStatesGroup.main_menu.set()
 
 
