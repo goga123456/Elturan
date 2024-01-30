e73370a8f6f9f0bf0c787882013bd2d721b527e4
goga123456 <110159417+goga123456@users.noreply.github.com>
2024-01-30 14:03:49 +0500

    Update main.py

--- main.py
+++ main.py
@@ -337,12 +337,12 @@ async def edu_keyboard(callback_query: types.CallbackQuery, state: FSMContext):
 
      
 
-        run_time = datetime.now() + timedelta(hours=12)
-        run_time1 = datetime.now() + timedelta(hours=4)
-        run_time2 = datetime.now() + timedelta(hours=12)
-        run_time3 = datetime.now() + timedelta(hours=24)
-        run_time4 = datetime.now() + timedelta(hours=72)
-        run_time5 = datetime.now() + timedelta(hours=168)
+        run_time = datetime.now() + timedelta(seconds=5)
+        run_time1 = datetime.now() + timedelta(seconds=10)
+        run_time2 = datetime.now() + timedelta(seconds=20)
+        run_time3 = datetime.now() + timedelta(seconds=30)
+        run_time4 = datetime.now() + timedelta(seconds=40)
+        run_time5 = datetime.now() + timedelta(seconds=50)
         if date[4] == 1:
             msg = bot.send_message(CHANNEL_ID, "@IsmoilovOybek")
             message_id = msg.message_id
