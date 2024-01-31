8893cc6294babae4a8b3a11fe0910fe48206fd0e
goga123456 <110159417+goga123456@users.noreply.github.com>
2024-01-31 11:25:11 +0500

    Update main.py

--- main.py
+++ main.py
@@ -69,6 +69,7 @@ def save_task_to_db(task_type, run_date, args):
         raise  # Re-raise the exception for further handling
 def delete_task(task_id):
     try:
+        """
         with conn, conn.cursor() as cursor:
             cursor.execute("SELECT id FROM scheduled_tasks WHERE args->>1 = %s", (task_id,))
             result = cursor.fetchone()
@@ -78,7 +79,7 @@ def delete_task(task_id):
                 scheduled_task = scheduled_tasks.pop(result, None)
                 if scheduled_task:
                     scheduled_task.remove()
-
+        """
         # Удаление задачи из базы данных
         with conn, conn.cursor() as cursor:
             cursor.execute("DELETE FROM scheduled_tasks WHERE args->>1 = %s", (task_id,))
