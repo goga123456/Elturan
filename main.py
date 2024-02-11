3fa1b564f1bd28a4ed4013d9cf8dbb27b40deb21
goga123456 <110159417+goga123456@users.noreply.github.com>
2024-02-11 16:55:13 +0500

    Update main.py

--- main.py
+++ main.py
@@ -78,15 +78,18 @@ async def delete_task(task_id):
         with conn, conn.cursor() as cursor:
             cursor.execute("SELECT id FROM scheduled_tasks WHERE args->>0 = %s", (task_id,))
             result = cursor.fetchone()
-            conn.commit() 
-            job_id = result[0]
-            print(job_id)     
-            scheduler.remove_job(job_id)
-          
-        # Удаление задачи из базы данных
-        with conn, conn.cursor() as cursor:
+            if result:
+                job_id = result[0]
+                print(f"Trying to delete job with ID: {job_id}")
+
+                # Проверка, существует ли задача перед удалением
+                if scheduler.get_job(job_id):
+                    scheduler.remove_job(job_id)
+                    print(f"Job {job_id} removed successfully")
+                else:
+                    print(f"No job with ID {job_id} was found in the scheduler")
             cursor.execute("DELETE FROM scheduled_tasks WHERE args->>0 = %s", (task_id,))
-            conn.commit()  # Commit the transaction
+            conn.commit()
 
     except psycopg2.Error as e:
         print(f"Error deleting task {task_id} from schedule:", e)
