3f715d41387ab3812df473cd699430fdbbb9870b
goga123456 <110159417+goga123456@users.noreply.github.com>
2024-02-12 13:40:21 +0500

    Update main.py

--- main.py
+++ main.py
@@ -80,7 +80,8 @@ async def delete_task(task_id):
             cursor.execute("SELECT id FROM scheduled_tasks WHERE args->>0 = %s", (task_id,))
             result = cursor.fetchone()
             if result:
-                job_id = uuid.UUID(result[0]).replace("-", "")
+                result[0].replace("-", "")
+                job_id = uuid.UUID(result[0])
                 print(f"Trying to delete job with ID: {job_id}")
                 print(job_id)
                 print(scheduler.get_job(job_id))
