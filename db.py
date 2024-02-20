0cb9f0d216249a90d05a3b9e972501b98fe97421
goga123456 <110159417+goga123456@users.noreply.github.com>
2024-02-20 15:02:01 +0500

    Update db.py

--- db.py
+++ db.py
@@ -19,27 +19,27 @@ class Database:
             await conn.close()
     
 
-    async def insert(self, inc_number, inc_category, desc, priority, status):
+    async def insert(self, inc_number, inc_category, desc, priority, status, created_at):
         conn = await self.connect()
         try:
             # Преобразование priority в целое число
             priority_int = int(priority)
 
             await conn.execute(
-                'INSERT INTO incidents (inc_number, inc_category, "desc", priority, status) VALUES ($1, $2, $3, $4, $5);',
-                inc_number, inc_category, desc, priority_int, status)
+                'INSERT INTO incidents (inc_number, inc_category, "desc", priority, created_at) VALUES ($1, $2, $3, $4, $5, $6);',
+                inc_number, inc_category, desc, priority_int, created_at)
         finally:
             await conn.close()
 
-    async def insert_deleted(self, inc_number, inc_category, desc, priority, status):
+    async def insert_deleted(self, inc_number, inc_category, desc, priority, status, created_at):
         conn = await self.connect()
         try:
             # Преобразование priority в целое число
             priority_int = int(priority)
 
             await conn.execute(
-                'INSERT INTO closed_incidents (inc_number, inc_category, "desc", priority, status) VALUES ($1, $2, $3, $4, $5);',
-                inc_number, inc_category, desc, priority_int, status)
+                'INSERT INTO closed_incidents (inc_number, inc_category, "desc", priority, status, created_at) VALUES ($1, $2, $3, $4, $5, $6);',
+                inc_number, inc_category, desc, priority_int, status, created_at)
         finally:
             await conn.close()   
 
