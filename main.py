b69ec8c5d3db3c25d3c41db1c16cf4428ded9e81
goga123456 <110159417+goga123456@users.noreply.github.com>
2024-03-05 17:26:12 +0500

    Update main.py

--- main.py
+++ main.py
@@ -471,55 +471,14 @@ async def edu_keyboard(callback_query: types.CallbackQuery, state: FSMContext):
             callback_query.data == '4' or callback_query.data == '5'):
         async with state.proxy() as data:
             data['priority'] = callback_query.data
-            await baza.insert(data['number'], data['category'], data['desc'], data['priority'], 'Открыто', datetime.now())
-            await bot.send_message(CHANNEL_ID, f"{data['category']}\n"
-                                               f"🆕ОТКРЫТ Инц. №{data['number']}\n"
-                                               f"Приоритет: {data['priority']}\n"
-                                               f"Описание: {data['desc']}\n")
-            run_time1 = datetime.now() + timedelta(hours=4)
-            run_time2 = datetime.now() + timedelta(hours=12)
-            run_time3 = datetime.now() + timedelta(hours=24)
-            run_time4 = datetime.now() + timedelta(hours=72)
-            run_time5 = datetime.now() + timedelta(hours=168)
-          
-            #task_uuid = str(uuid.uuid4())
-            if data['priority'] == '1':
-                job=scheduler.add_job(prosrochen, "date", run_date=run_time1, 
-                              args=[data['number'], data['priority'], data['category'], data['desc']],
-                              max_instances=1)
-                await print_all_jobs()
-                save_task_to_db(job.id, 'prosrochen', run_time1, [data['number'], data['priority'], data['category'], data['desc']])
-            if data['priority'] == '2':
-                job=scheduler.add_job(prosrochen, "date", run_date=run_time2,
-                              args=[data['number'], data['priority'], data['category'], data['desc']],
-                              max_instances=1)
-                await print_all_jobs()
-                save_task_to_db(job.id, 'prosrochen', run_time2, [data['number'], data['priority'], data['category'], data['desc']])
-            if data['priority'] == '3':
-                job=scheduler.add_job(prosrochen, "date", run_date=run_time3,
-                              args=[data['number'], data['priority'], data['category'], data['desc']],
-                              max_instances=1)
-                await print_all_jobs()
-                save_task_to_db(job.id, 'prosrochen', run_time3, [data['number'], data['priority'], data['category'], data['desc']])
-            if data['priority'] == '4':
-                job=scheduler.add_job(prosrochen, "date", run_date=run_time4,
-                              args=[data['number'], data['priority'], data['category'], data['desc']],
-                              max_instances=1)
-                save_task_to_db(job.id, 'prosrochen', run_time4, [data['number'], data['priority'], data['category'], data['desc']])
-            if data['priority'] == '5':
-                job=scheduler.add_job(prosrochen, "date", run_date=run_time5,
-                              args=[data['number'], data['priority'], data['category'], data['desc']],
-                              max_instances=1)
-                save_task_to_db(job.id, 'prosrochen', run_time5, [data['number'], data['priority'], data['category'], data['desc']])
+
         await callback_query.message.delete()
         await bot.send_message(chat_id=callback_query.message.chat.id,
-                               text=callback_query.data, reply_markup=create_incident_kb())      
+                               text=callback_query.data)
         await bot.send_message(callback_query.message.chat.id, 
-                                               f"{data['category']}\n"
-                                               f"🆕ОТКРЫТ Инц. №{data['number']}\n"
-                                               f"Приоритет: {data['priority']}\n"
-                                               f"Описание: {data['desc']}\n")      
-        await ProfileStatesGroup.main_menu.set()
+                                               "Если вы знаете причину ,можете добавить её",
+                               reply_markup=cause_kb())
+        await ProfileStatesGroup.cause.set()
     if callback_query.data == 'Back':
         async with state.proxy() as data:
             await callback_query.message.delete()
