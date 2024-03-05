277e7ccb0d22d79ae29ba866480fe78768d45de7
goga123456 <110159417+goga123456@users.noreply.github.com>
2024-03-05 17:31:29 +0500

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from aiogram.dispatcher import FSMContext
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from config import TOKEN_API
-from markups.inline_markups import priority_kb, edit_kb, inc_category_kb
+from markups.inline_markups import priority_kb, edit_kb, inc_category_kb, cause_kb
 from markups.reply_markups_start_and_back import *
 from states import ProfileStatesGroup
 import os
