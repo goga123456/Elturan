22c517b8d0823a5485284ca450bc0f0dba6d6aeb
goga123456 <110159417+goga123456@users.noreply.github.com>
2024-04-26 07:41:15 +0300

    Update db.py

--- db.py
+++ db.py
@@ -13,123 +13,72 @@ class Database:
         ssl_context.verify_mode = ssl.CERT_NONE
         return await asyncpg.connect(dsn=self.db_url, ssl=ssl_context)
 
-
     async def is_inc_number_unique(self, inc_number):
-        conn = await self.connect()
-        try:
-            # Проверяем, существует ли запись с таким inc_number
+        async with await self.connect() as conn:
             result = await conn.fetchrow('SELECT * FROM incidents WHERE inc_number = $1', inc_number)
-            return result is None  # Возвращаем True, если такой номер не найден (уникален)
-        finally:
-            await conn.close()
-    
+            return result is None
 
     async def insert(self, inc_number, inc_category, desc, priority, status, created_at, cause):
-        conn = await self.connect()
-        try:
-            # Преобразование priority в целое число
+        async with await self.connect() as conn:
             priority_int = int(priority)
-
             await conn.execute(
                 'INSERT INTO incidents (inc_number, inc_category, "desc", priority, created_at, cause) VALUES ($1, $2, $3, $4, $5, $6);',
                 inc_number, inc_category, desc, priority_int, created_at, cause)
-        finally:
-            await conn.close()
 
     async def insert_deleted(self, inc_number, inc_category, desc, priority, status, created_at, cause):
-        conn = await self.connect()
-        try:
-            # Преобразование priority в целое число
+        async with await self.connect() as conn:
             priority_int = int(priority)
-
             await conn.execute(
                 'INSERT INTO closed_incidents (inc_number, inc_category, "desc", priority, status, created_at, cause) VALUES ($1, $2, $3, $4, $5, $6, $7);',
                 inc_number, inc_category, desc, priority_int, status, created_at, cause)
-        finally:
-            await conn.close()   
 
     async def incidents(self):
-        conn = await self.connect()
-        try:
+        async with await self.connect() as conn:
             result = await conn.fetch('SELECT inc_number FROM incidents')
             return result
-        finally:
-            await conn.close()
 
     async def closed_incidents(self):
-        conn = await self.connect()
-        try:
+        async with await self.connect() as conn:
             result = await conn.fetch('SELECT inc_number FROM closed_incidents')
             return result
-        finally:
-            await conn.close()
 
     async def select_incident(self, inc_number):
-        conn = await self.connect()
-        try:
+        async with await self.connect() as conn:
             result = await conn.fetchrow('SELECT * FROM incidents WHERE inc_number = $1', inc_number)
             return result
-        finally:
-            await conn.close()
 
     async def select_closed_incident(self, inc_number):
-        conn = await self.connect()
-        try:
+        async with await self.connect() as conn:
             result = await conn.fetchrow('SELECT * FROM closed_incidents WHERE inc_number = $1', inc_number)
             return result
-        finally:
-            await conn.close()
-    
 
     async def select_priority(self, inc_number):
-        conn = await self.connect()
-        try:
+        async with await self.connect() as conn:
             result = await conn.fetchrow('SELECT priority FROM incidents WHERE inc_number = $1', inc_number)
             return result[0]
-        finally:
-            await conn.close()
 
     async def select_created_date(self, inc_number):
-        conn = await self.connect()
-        try:
+        async with await self.connect() as conn:
             result = await conn.fetchrow('SELECT created_at FROM incidents WHERE inc_number = $1', inc_number)
-            timestamp = result[0]
-            return timestamp
-        finally:
-            await conn.close()
+            return result[0]
 
     async def delete_incident(self, inc_number):
-        conn = await self.connect()
-        try:
+        async with await self.connect() as conn:
             await conn.execute('DELETE FROM incidents WHERE inc_number = $1', inc_number)
-        finally:
-            await conn.close()
 
     async def delete_incident_from_deleted(self, inc_number):
-        conn = await self.connect()
-        try:
+        async with await self.connect() as conn:
             await conn.execute('DELETE FROM closed_incidents WHERE inc_number = $1', inc_number)
-        finally:
-            await conn.close()
-    
+
     async def update_status(self, status, inc_number):
-        conn = await self.connect()
-        try:
+        async with await self.connect() as conn:
             await conn.execute("UPDATE incidents SET status = $1 WHERE inc_number = $2", status, inc_number)
-        finally:
-            await conn.close()
 
     async def update_priority(self, priority, inc_number):
-        conn = await self.connect()
-        try:
+        async with await self.connect() as conn:
             await conn.execute("UPDATE incidents SET priority = $1 WHERE inc_number = $2", priority, inc_number)
-        finally:
-            await conn.close()
 
     async def update_description(self, desc, inc_number):
-        conn = await self.connect()
-        try:
+        async with await self.connect() as conn:
             await conn.execute('UPDATE incidents SET "desc" = $1 WHERE inc_number = $2', desc, inc_number)
-        finally:
-            await conn.close()
 
